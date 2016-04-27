2da5a6570a2c57917e8497c49755286be7fc9c66
Leonardo Agueci <leo@MBP-di-Leonardo.lan>
2016-04-27 20:54:32 +0200

    boh

--- MaPS/app/src/main/java/com/maps/unipi/maps/MainActivity.java
+++ MaPS/app/src/main/java/com/maps/unipi/maps/MainActivity.java
@@ -1,35 +1,107 @@
 package com.maps.unipi.maps;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final int duration = Toast.LENGTH_SHORT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button login = (Button) findViewById(R.id.main_b_login);
-        Button join  = (Button) findViewById(R.id.main_b_join);
-        login.setOnClickListener(this);
-        join.setOnClickListener(this);
     }
 
-    @Override
-    public void onClick(View v){
+    public void onClickLogin(View v) {
+        CharSequence buff, tmp;
+        String buffPass, buffCard;
 
-        if(v.getId() == R.id.main_b_login){
+        int i;
+        CharSequence msg;
+        Toast toast;
+        Resources myRes = getResources();
+        Context context = getApplicationContext();
 
+        EditText logCard = (EditText) findViewById(R.id.main_et_card);
+        EditText logPass = (EditText) findViewById(R.id.main_et_pass);
+
+
+        File directory = Environment.getExternalStorageDirectory(); //not necessarily the SD card path (!)
+        File regFile = new File(directory.getAbsolutePath() + "/MaPS/prova.txt");
+
+        try {
+            CharSequence card = logCard.getText();
+            CharSequence pass = logPass.getText();
+            if(pass.toString().equals("") || card.toString().equals("")){
+                msg = myRes.getText(R.string.unsuccess);
+                toast = Toast.makeText(context, msg, duration);
+                toast.show();
+                return;
+            }
+
+            FileReader f = new FileReader(regFile);
+            BufferedReader br = new BufferedReader(f);
+
+            while (true) {
+                buff = br.readLine();
+                if (buff == null)
+                    break;
+
+                i = buff.length();
+
+                buffPass = buffCard = "";
+                while (!Character.isWhitespace(buff.charAt(--i)))
+                    buffPass = buff.charAt(i) + buffPass;
+                Log.d("debug", buffPass);
+
+                while (!Character.isWhitespace(buff.charAt(--i)))
+                    buffCard = buff.charAt(i) + buffCard;
+                Log.d("debug", buffCard);
+
+                if (buffCard.contentEquals(card.toString()) && buffPass.contentEquals(pass.toString())) {
+
+                    msg = myRes.getText(R.string.success);
+                    toast = Toast.makeText(context, msg, duration);
+                    toast.show();
+                    br.close();
+                    f.close();
+                    return; //TODO gestione nuova activity
+                }
+            }
+            Log.d("debug", "1");
+            msg = myRes.getText(R.string.unsuccess);
+            toast = Toast.makeText(context, msg, duration);
+            toast.show();
+            logCard.setText(null);
+            logPass.setText(null);
+            br.close();
+            f.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        else if(v.getId() == R.id.main_b_join){
+    }
 
-            Intent registration_form = new Intent(this, Registration.class);
-            startActivity(registration_form);
-        }
-
+    public void onClickJoin(View v){
+        Intent registration_form = new Intent(this, Registration.class);
+        startActivity(registration_form);
     }
 }
--- MaPS/app/src/main/java/com/maps/unipi/maps/Registration.java
+++ MaPS/app/src/main/java/com/maps/unipi/maps/Registration.java
@@ -1,41 +1,105 @@
 package com.maps.unipi.maps;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.io.*;
 
-public class Registration extends AppCompatActivity implements View.OnClickListener {
+public class Registration extends AppCompatActivity{
+
+    private final int duration = Toast.LENGTH_SHORT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
-        Button done = (Button) findViewById(R.id.reg_b_done);
-        done.setOnClickListener(this);
     }
 
-    @Override
-    public void onClick(View v) {
+    /* Checks if external storage is available for read and write */
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
+    }
 
+    public void onClickDone(View v) {
+        Context context = getApplicationContext();
+        CharSequence msg;
+        Toast toast;
+        Resources myRes = getResources();
 
+        EditText regName = (EditText) findViewById(R.id.reg_et_name);
+        EditText regSurname = (EditText) findViewById(R.id.reg_et_surname);
+        EditText regCard = (EditText) findViewById(R.id.reg_et_card);
+        EditText regPass = (EditText) findViewById(R.id.reg_et_pass);
+        EditText regConfpass = (EditText) findViewById(R.id.reg_et_confpass);
 
-        EditText reg_name = (EditText) findViewById(R.id.reg_et_name);
-        String name = reg_name.getText().toString();
+        CharSequence name = regName.getText();
+        CharSequence surname = regSurname.getText();
+        CharSequence card = regCard.getText();
+        CharSequence pass = regPass.getText();
+        CharSequence confpass = regConfpass.getText();
+
+        if(name.toString().equals("") || surname.toString().equals("") || card.toString().equals("") || pass.toString().equals("") || confpass.toString().equals("")){
+            msg = myRes.getText(R.string.unsuccess);
+            toast = Toast.makeText(context, msg, duration);
+            toast.show();
+            return;
+        }
+
+        CharSequence toSubmit = name + " " + surname + " " + card + " " + pass +"\n";
+
+        if(!pass.toString().contentEquals(confpass.toString())){
+
+            msg = myRes.getText(R.string.passmiss);
+            toast = Toast.makeText(context, msg, duration);
+            toast.show();
+            regPass.setText(null);
+            regConfpass.setText(null);
+            return;
+        }
+
+        if(!this.isExternalStorageWritable()){
+
+            Log.d("Registration", myRes.getText(R.string.exnotava).toString());
+            return;
+        }
+
+        // Setup the folder for the log file
+        File directory = Environment.getExternalStorageDirectory(); //not necessarily the SD card path (!)
+        File logDirectory = new File(directory.getAbsolutePath() + "/MaPS");
+        logDirectory.mkdirs();
 
         try{
-
-            FileOutputStream f = openFileOutput("prova.txt", Context.MODE_PRIVATE);
-            f.write(name.getBytes());
+            File regFile = new File (logDirectory, "prova.txt");
+            FileOutputStream f = new FileOutputStream(regFile, true);
+            OutputStreamWriter sw = new OutputStreamWriter(f);
+            sw.append(toSubmit);
+            sw.close();
             f.close();
         }
-        catch (IOException e){}
+        catch (IOException e){
+            e.printStackTrace();
+        }
+
+        msg = myRes.getText(R.string.success);
+        toast = Toast.makeText(context, msg, duration);
+        toast.show();
+
+
 
     }
+
+
 }
